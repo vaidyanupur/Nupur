af3fcf33a039d794cc713dbd16c807e6d15700c8
Hp <Hp@Nupur-Lappy>
2019-05-18 12:27:10 +0530

    TestNgMaven

---
+++ TestNgMaven/src/main/java/TestNgDemo/TestNgMaven/App.java
@@ -0,0 +1,13 @@
+package TestNgDemo.TestNgMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
